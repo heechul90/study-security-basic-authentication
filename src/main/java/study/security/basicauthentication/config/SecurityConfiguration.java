e6d4151da2353261fe318f86072f34e99d13f839
hclee <heechul4296@gmail.com>
2022-10-15 00:14:47 +0900

    basic 인증 기본 테스트

---
+++ src/main/java/study/security/basicauthentication/config/SecurityConfiguration.java
@@ -0,0 +1,38 @@
+package study.security.basicauthentication.config;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+
+@EnableWebSecurity
+//@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser(
+                        User.withDefaultPasswordEncoder()
+                                .username("user")
+                                .password("1234")
+                                .roles("USER")
+                                .build()
+                );
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+
+
+}
--- src/test/java/study/security/basicauthentication/BasicAuthenticationApplicationTests.java
+++ src/test/java/study/security/basicauthentication/BasicAuthenticationApplicationTests.java
@@ -1,13 +1,69 @@
 package study.security.basicauthentication;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
 
-@SpringBootTest
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class BasicAuthenticationApplicationTests {
 
-    @Test
-    void contextLoads() {
+    @LocalServerPort
+    int port;
+
+    RestTemplate client = new RestTemplate();
+
+    private String greetingUrl() {
+        return "http://localhost:" + port + "/greeting";
     }
 
+    @Test
+    @DisplayName("인증 실패")
+    void authenticated_fail() {
+        //String response = client.getForObject(greetingUrl(), String.class);
+        assertThatThrownBy(() -> client.getForObject(greetingUrl(), String.class))
+                .isInstanceOf(HttpClientErrorException.class);
+    }
+
+    @Test
+    @DisplayName("인증 성공1")
+    void authenticated_success1() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.AUTHORIZATION, "Basic " + Base64.getEncoder().encodeToString(
+                "user:1234".getBytes()
+        ));
+        HttpEntity entity = new HttpEntity(null, headers);
+        ResponseEntity<String> response = client.exchange(greetingUrl(), HttpMethod.GET, entity, String.class);
+
+        assertThat(response.getBody()).isEqualTo("hello");
+    }
+
+    @Test
+    @DisplayName("인증 성공2")
+    void authenticated_success2() {
+        TestRestTemplate testClient = new TestRestTemplate("user", "1234");
+        String response = testClient.getForObject(greetingUrl(), String.class);
+        assertThat(response).isEqualTo("hello");
+    }
+
+    @Test
+    @DisplayName("POST 인증")
+    void authenticated_success3_post() {
+        TestRestTemplate testClient = new TestRestTemplate("user", "1234");
+        ResponseEntity<String> response = testClient.postForEntity(greetingUrl(), "heech", String.class);
+        assertThat(response.getBody()).isEqualTo("hello heech");
+    }
 }
